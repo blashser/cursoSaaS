949c63606c11e99831694ad1c9e6ed425ae230d9
Sergio Hernández <blashser.aero@gmail.com>
2012-10-09 00:34:31 +0200

    Add more palindromos
    Add Spanish palindromos. Therefore utf-8 coding is neccesary.
    
    Improve test part with an array of string with palindromos
    and no palindromos string.

--- hw1/part1.rb
+++ hw1/part1.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #For those just starting out, one suggested way to work on your code is to have a command window open and a text editor with this
 #file loaded.  Make changes to this file and then run 'ruby part1.rb' at the command line, this will run your program.  Once you're
 #satisfied with your work, save your file and upload it to the checker.
@@ -14,22 +15,22 @@ end
 
 #the code below this line will test your functions.  You should remove everything below this line prior to submitting your file
 
+palin = [ "Dábale arroz a la zorra el abad.",
+          "¿Acaso hubo búhos acá?",
+          "No lata, no: la totalidad arada dilato talón a talón.",
+          "A man, a plan, a canal: Panama!",
+          "Madam, in Eden I'm Adam" ]
 
-test_str = "there goes the neighborhood"
+no_palin = [ "there goes the neighborhood" ]
 
-if palindrome? test_str
-  puts test_str + " is a palindrome!"
-else
-  puts test_str + " is NOT a palindrome!"
-end
+test_string = palin + no_palin
 
-
-test_str = "Madam, I'm Adam"
-
-if palindrome? test_str
-  puts test_str " is a palindrome!"
-else
-  puts test_str " is NOT a palindrome!"
+test_string.each do |str|
+  if palindrome? str
+    puts str + " is a palindrome!"
+  else
+    puts str + " is NOT a palindrome!"
+  end
 end
 
 
