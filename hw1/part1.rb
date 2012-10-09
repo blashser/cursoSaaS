c7c8168180feb9bcaf35eb8286806fe71a236031
Sergio Hernández <blashser.aero@gmail.com>
2012-10-09 02:18:24 +0200

    Remove part used to test
    Before submit files to check it is said that test
    part must be removed.

--- hw1/part1.rb
+++ hw1/part1.rb
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-#For those just starting out, one suggested way to work on your code is to have a command window open and a text editor with this
-#file loaded.  Make changes to this file and then run 'ruby part1.rb' at the command line, this will run your program.  Once you're
-#satisfied with your work, save your file and upload it to the checker.
 
 def variations_a_es( str )
   str = str.gsub( /á/, "a" )
@@ -82,39 +79,3 @@ def count_words(str)
   end
   hash_str
 end
-
-
-#the code below this line will test your functions.  You should remove everything below this line prior to submitting your file
-
-palin = [ "Dábale arroz a la zorra el abad.",
-          "¿Acaso hubo búhos acá?",
-          "No lata, no: la totalidad arada dilato talón a talón.",
-          "A man, a plan, a canal: Panama!",
-          "Madam, in Eden I'm Adam" ]
-
-no_palin = [ "there goes the neighborhood" ]
-
-test_string = palin + no_palin
-
-test_string.each do |str|
-  if palindrome? str
-    puts str + " is a palindrome!"
-  else
-    puts str + " is NOT a palindrome!"
-  end
-end
-
-
-test_str = [ "The rent is due on the first day of the month unless the first day of the month falls on a Saturday or Sunday",
-             "I'm Sergio. I'm doing homework!Do   you not believe me?",
-             "This is to test the middle-line. I'm testing if middle-line is taken as one unique word." ]
-
-test_str.each do |test_st|
-  word_count = count_words test_st
-  puts word_count
-  word_count.keys.each do |key|
-    print key + " => " + word_count[ key ].to_s + ", "
-  end
-  print "\n"
-end
-
