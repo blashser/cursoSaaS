8749fc332f2ee0c0aac9233b825262b68ade5e22
Sergio Hernández <blashser.aero@gmail.com>
2012-10-09 03:34:57 +0200

    Prepare test part
    Ready to test the logic to implement.

--- hw1/part2.rb
+++ hw1/part2.rb
@@ -12,3 +12,26 @@ end
 def rps_tournament_winner(tournament)
   # YOUR CODE HERE
 end
+
+
+### Test part ###
+
+if __FILE__ == $0
+  list = [ ["Armando", "R"], ["Dave", "R"] ] # Dave would win since S > P
+
+  winer = rps_game_winner list
+  
+  puts winer + " wins!"
+
+  tournament = [
+                [
+                 [ ["Armando", "P"], ["Dave", "S"] ],
+                 [ ["Richard", "R"],  ["Michael", "S"] ],
+                ],
+                [
+                 [ ["Allen", "S"], ["Omer", "P"] ],
+                 [ ["David E.", "R"], ["Richard X.", "P"] ]
+                ]
+               ]
+  rps_tournament_winner tournament
+end
