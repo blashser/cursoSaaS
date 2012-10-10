75e9e2b9c491a78e4d1c2ae48ccc41b72d63f3a0
Sergio Hernández <blashser.aero@gmail.com>
2012-10-10 20:40:43 +0200

    First version of JellyBean class
    Do as it is told in HW1 of SaaS course.
    Inherite from Dessert class
    Add getter and setter for flavor member.
    Modify method delicious? returns false if flavor is "black licorice".

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -18,11 +18,15 @@ end
 
 class JellyBean < Dessert
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    @name     = name
+    @calories = calories
+    @flavor   = flavor
   end
   
+  attr_accessor :flavor
+
   def delicious?
-    # YOUR CODE HERE
+    true unless "black licorice" == flavor
   end
 end
 
@@ -32,6 +36,9 @@ if __FILE__ == $0
 
   yogurt = Dessert.new "Plátano", 180
   pastel = Dessert.new "Pastel de Queso", 400
+
+  jellybn = JellyBean.new "caramelo", 300, "black licorice"
+  nojellb = JellyBean.new "pera", 100, "fruta"
 
 end
 
