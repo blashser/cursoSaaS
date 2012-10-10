bccde1746e3f73b459e582fb39036fb2905c020d
Sergio Hernández <blashser.aero@gmail.com>
2012-10-10 21:35:19 +0200

    Testing classes
    Trying to check the classes.

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -1,31 +1,38 @@
 # -*- coding: utf-8 -*-
 class Dessert
   def initialize(name, calories)
+    puts "creando dessert"
     @name     = name
     @calories = calories
+    puts "dessert creado"
   end
 
   attr_accessor :name, :calories
   
   def healthy?
+    puts "hola healthy"
     @calories < 200
   end
   
   def delicious?
+    puts "testing delicious in dessert"
     true
   end
 end
 
 class JellyBean < Dessert
   def initialize(name, calories, flavor)
+    puts "creando jellybean"
     @name     = name
     @calories = calories
     @flavor   = flavor
+    puts "jellybean creado"
   end
   
   attr_accessor :flavor
 
   def delicious?
+    puts "testing delicious in JELLYBEAN"
     true unless "black licorice" == flavor
   end
 end
@@ -34,11 +41,25 @@ end
 
 if __FILE__ == $0
 
-  yogurt = Dessert.new "Plátano", 180
+  yogurt = Dessert.new  180,"Plátano"
   pastel = Dessert.new "Pastel de Queso", 400
 
   jellybn = JellyBean.new "caramelo", 300, "black licorice"
   nojellb = JellyBean.new "pera", 100, "fruta"
+
+  print "jellybn"
+  if jellybn.delicious? 
+    puts " is delicious!"
+  else
+    puts " is NOT delicious!"
+  end
+
+  print "nojellybn"
+  if nojellb.delicious? 
+    puts " is delicious!"
+  else
+    puts " is NOT delicious!"
+  end
 
 end
 
