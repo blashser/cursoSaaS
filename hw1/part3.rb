769d1b6fbf6b94f751a09150141df8544ebd53e0
Sergio Hernández <blashser.aero@gmail.com>
2012-10-10 16:55:58 +0200

    Remove unless logic
    This is no more necessary because word repeted are
    removed of words_to_test.

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -5,22 +5,21 @@ def combine_anagrams(words)
 
   words.each do |anag1|
     group = []
-    unless group_anagrams.flatten.include? anag1
-      words_to_test.each do |test|
-        if anag1.downcase.split(//).sort.join == test.downcase.split(//).sort.join
-          puts anag1 + " and " + test + " are anagrams or equals."
-          group << test 
-        end
+
+    words_to_test.each do |test|
+      if anag1.downcase.split(//).sort.join == test.downcase.split(//).sort.join
+        puts anag1 + " and " + test + " are anagrams or equals."
+        group << test 
       end
-      puts group_anagrams.inspect
-      puts group.flatten.at( 0 )
+    end
+    puts group_anagrams.inspect
+    puts group.flatten.at( 0 )
 
-      group_anagrams << group unless group.empty?
-
-      unless group.empty?
-        group.each do |to_remove|
-          words_to_test.delete to_remove
-        end
+    group_anagrams << group unless group.empty?
+    
+    unless group.empty?
+      group.each do |to_remove|
+        words_to_test.delete to_remove
       end
     end
   end
