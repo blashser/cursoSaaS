91ea485a40c303fed74f7adeabe1ed73e71a5c1e
Sergio Hernández <blashser.aero@gmail.com>
2012-10-10 15:06:30 +0200

    Develop test part of anagrams program
    Prepare part to test combine_anagrams method.

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -1,3 +1,14 @@
 def combine_anagrams(words)
   words
 end
+
+#---------test part--------#
+
+if __FILE__ == $0
+
+    string_with_anagrams = ['Cars', 'FOR', 'potatoes', 'racs', 'four','scar', 'Creams', 'screaM']
+
+    anagrams_in_group = combine_anagrams string_with_anagrams
+    
+    puts anagrams_in_group.inspect
+end
