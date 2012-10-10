ff18d4a198f3057dbe550b712dfd278554a65ac4
Sergio Hernández <blashser.aero@gmail.com>
2012-10-10 15:39:21 +0200

    Detect if words are anagrams
    logic to split string and sorting array and join afterwards.

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -1,5 +1,24 @@
 def combine_anagrams(words)
-  words
+  group_anagrams = []
+  words.each do |anag1|
+
+    group_anagrams << anag1
+
+    if group_anagrams.length > 0
+      group_anagrams.each do |test|
+        print anag1.downcase.split(//).sort
+        puts
+        print test.downcase.split(//).sort
+        puts
+
+        if anag1.downcase.split(//).sort.join == test.downcase.split(//).sort.join
+          puts anag1 + " and " + test + " are anagrams or equals."
+        else
+          puts anag1 + " and " + test + " are NOTHING."
+        end
+      end
+    end
+  end
 end
 
 #---------test part--------#
