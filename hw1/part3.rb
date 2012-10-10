1fe28d2b2ce7d98c55ad28196f0b359502db6cb7
Sergio Hernández <blashser.aero@gmail.com>
2012-10-10 16:46:48 +0200

    Remove anagrams repetitions
    logic is reimplemented.
    Remove words detected from words_to_test array.

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -5,17 +5,27 @@ def combine_anagrams(words)
 
   words.each do |anag1|
     group = []
-    words_to_test.each do |test|
+    unless group_anagrams.flatten.include? anag1
+      words_to_test.each do |test|
+        puts "Comparing " + anag1 + " and " + test
+        if anag1.downcase.split(//).sort.join == test.downcase.split(//).sort.join
+          puts anag1 + " and " + test + " are anagrams or equals."
+          group << test 
+        else
+          # puts anag1 + " and " + test + " are NOTHING."
+        end
+      end
+      puts group_anagrams.flatten.inspect
+      puts group.flatten.at( 0 )
 
-      if anag1.downcase.split(//).sort.join == test.downcase.split(//).sort.join
-        puts anag1 + " and " + test + " are anagrams or equals."
-        group << test
-      else
-#        puts anag1 + " and " + test + " are NOTHING."
+      group_anagrams << group unless group.empty?
+
+      unless group.empty?
+        group.each do |to_remove|
+          words_to_test.delete to_remove
+        end
       end
     end
-    group_anagrams << group
-    words_to_test.shift # Remove first element. No compare next element with itself
   end
   group_anagrams
 end
