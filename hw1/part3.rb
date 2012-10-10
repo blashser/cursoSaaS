5510f9ef15e8fdff78499481eefe6d1512356e55
Sergio Hernández <blashser.aero@gmail.com>
2012-10-10 16:05:19 +0200

    Create group of anagrams
    But it has repetitions.
    This is not a final version.

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -1,31 +1,30 @@
 def combine_anagrams(words)
   group_anagrams = []
+  
+  words_to_test = Array.new words
+
   words.each do |anag1|
+    group = []
+    words_to_test.each do |test|
 
-    group_anagrams << anag1
-
-    if group_anagrams.length > 0
-      group_anagrams.each do |test|
-        print anag1.downcase.split(//).sort
-        puts
-        print test.downcase.split(//).sort
-        puts
-
-        if anag1.downcase.split(//).sort.join == test.downcase.split(//).sort.join
-          puts anag1 + " and " + test + " are anagrams or equals."
-        else
-          puts anag1 + " and " + test + " are NOTHING."
-        end
+      if anag1.downcase.split(//).sort.join == test.downcase.split(//).sort.join
+        puts anag1 + " and " + test + " are anagrams or equals."
+        group << test
+      else
+#        puts anag1 + " and " + test + " are NOTHING."
       end
     end
+    group_anagrams << group
+    words_to_test.shift # Remove first element. No compare next element with itself
   end
+  group_anagrams
 end
 
 #---------test part--------#
 
 if __FILE__ == $0
 
-    string_with_anagrams = ['Cars', 'FOR', 'potatoes', 'racs', 'four','scar', 'Creams', 'screaM']
+    string_with_anagrams = ['Cars', 'FOR', 'potatoes', 'racs', 'four','scar', 'Creams', 'screaM' , 'toy' ]
 
     anagrams_in_group = combine_anagrams string_with_anagrams
     
