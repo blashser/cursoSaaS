56f6301f5b8ccecd9697633c09899f444fc6705d
Sergio Hernández <blashser.aero@gmail.com>
2012-10-10 23:37:29 +0200

    Change prints
    Check constructor and access to instance method.

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -1,38 +1,37 @@
 # -*- coding: utf-8 -*-
 class Dessert
   def initialize(name, calories)
-    puts "creando dessert"
+    print "creando dessert..."
     @name     = name
     @calories = calories
-    puts "dessert creado"
+    puts "...dessert creado"
   end
 
   attr_accessor :name, :calories
   
   def healthy?
-    puts "hola healthy"
     @calories < 200
   end
   
   def delicious?
-    puts "testing delicious in dessert"
+    print "testing delicious in dessert"
     true
   end
 end
 
 class JellyBean < Dessert
   def initialize(name, calories, flavor)
-    puts "creando jellybean"
+    print "creando jellybean..."
     @name     = name
     @calories = calories
     @flavor   = flavor
-    puts "jellybean creado"
+    puts "...jellybean creado"
   end
   
   attr_accessor :flavor
 
   def delicious?
-    puts "testing delicious in JELLYBEAN"
+    print "testing delicious in JELLYBEAN"
     true unless "black licorice" == flavor
   end
 end
