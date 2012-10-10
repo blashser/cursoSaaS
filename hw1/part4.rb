979983c5a866ba0694479c3d5acb187d99ec95eb
Sergio Hernández <blashser.aero@gmail.com>
2012-10-10 23:48:13 +0200

    Remove part used to test
    Before submit files to check it is said that test
    part must be removed.

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -29,26 +29,6 @@ class JellyBean < Dessert
   end
 end
 
-#---------------test---------------#
 
-if __FILE__ == $0
-
-  yogurt = Dessert.new  "Plátano", 180
-  pastel = Dessert.new  "Pastel de Queso", 400
-
-  jellybn = JellyBean.new "caramelo", 300, "black licorice"
-  nojellb = JellyBean.new "pera", 100, "fruta"
-
-  puts
-  [ yogurt, pastel, jellybn, nojellb ].each do |dess|
-    puts dess.name
-    puts dess.calories
-    if dess.healthy? 
-      puts " is healthy!" 
-    end
-    puts " and is DELICIOUS" if dess.delicious?
-    puts
-  end
-end
 
 
