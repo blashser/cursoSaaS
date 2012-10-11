69824cdafa5fd0aa49ea2b4ef0c57a5f68830133
Sergio Hernández <blashser.aero@gmail.com>
2012-10-11 03:28:48 +0200

    First develop version
    Work but need to be testing deeply yet.

--- hw1/part5.rb
+++ hw1/part5.rb
@@ -1,8 +1,49 @@
 class Class
   def attr_accessor_with_history(attr_name)
-    attr_name = attr_name.to_s   # make sure it's a string
-    attr_reader attr_name        # create the attribute's getter
-    attr_reader attr_name+"_history" # create bar_history getter
-    class_eval "your code here, use %Q for multiline strings"
+    attr_name  = attr_name.to_s                # make sure it's a string
+    attr_reader  attr_name.to_sym              # create the attribute's getter
+    attr_reader (attr_name+"_history").to_sym  # create bar_history getter
+#   class_eval "your code here, use %Q for multiline strings"
+    class_eval %Q{
+      def initialize
+        @#{attr_name}_history = [nil]
+      end
+      def #{attr_name}_history=( new_name )
+        puts "HOLA HOLA HOLA"
+        puts #{attr_name}_history.to_s
+      end
+    }
+    class_eval %Q{
+      def #{attr_name}=( new_name )
+        @#{attr_name} = new_name
+        #{attr_name}_history << new_name
+        puts "#{attr_name}_history = [ " + new_name.to_s + " ]"
+        puts #{attr_name}_history.to_s
+      end
+    }
   end
+end
+
+
+class Foo
+  attr_accessor_with_history :bar
+end
+
+#---------------test-------------#
+
+if __FILE__ == $0
+  sergio = Foo.new
+
+  sergio.bar = 5
+  puts sergio.bar_history.inspect
+  sergio.bar = "mi casa"
+  puts sergio.bar_history.inspect
+  sergio.bar = [ 5, 6, 9 ]
+  puts sergio.bar_history.inspect
+  sergio.bar = 10
+  puts sergio.bar_history.inspect
+  
+  sergio.bar_history = 4
+
+  puts sergio.bar_history.inspect
 end
