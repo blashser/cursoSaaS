4a6dd5bb26a8d3a440f31a73962b603b602eff7b
Sergio Hernández <blashser.aero@gmail.com>
2012-10-12 23:28:15 +0200

    Adapt palindrome solution of exercise one
    Allow that "String" class has "palindrome?" method.

--- hw1/part6.rb
+++ hw1/part6.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class Numeric
   @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.0}
   def method_missing(method_id)
@@ -25,6 +26,83 @@ class Numeric
   end
 end
 
+  def variations_a_es( str )
+    str = str.gsub( /á/, "a" )
+    str = str.gsub( /à/, "a" )
+    str = str.gsub( /ä/, "a" )
+    str = str.gsub( /Á/, "a" )
+    str = str.gsub( /À/, "a" )
+    str = str.gsub( /Ä/, "a" )
+  end
+
+  def variations_e_es( str )
+    str = str.gsub( /é/, "e" )
+    str = str.gsub( /è/, "e" )
+    str = str.gsub( /ë/, "e" )
+    str = str.gsub( /É/, "e" )
+    str = str.gsub( /È/, "e" )
+    str = str.gsub( /Ë/, "e" )
+  end
+
+  def variations_i_es( str )
+    str = str.gsub( /í/, "i" )
+    str = str.gsub( /ì/, "i" )
+    str = str.gsub( /ï/, "i" )
+    str = str.gsub( /Í/, "i" )
+    str = str.gsub( /Ì/, "i" )
+    str = str.gsub( /Ï/, "i" )
+  end
+
+  def variations_o_es( str )
+    str = str.gsub( /ó/, "o" )
+    str = str.gsub( /ò/, "o" )
+    str = str.gsub( /ö/, "o" )
+    str = str.gsub( /Ó/, "o" )
+    str = str.gsub( /Ò/, "o" )
+    str = str.gsub( /Ö/, "o" )
+  end
+
+  def variations_u_es( str )
+    str = str.gsub( /ú/, "u" )
+    str = str.gsub( /ù/, "u" )
+    str = str.gsub( /ü/, "u" )
+    str = str.gsub( /Ú/, "u" )
+    str = str.gsub( /Ù/, "u" )
+    str = str.gsub( /Ü/, "u" )
+  end
+
+
+class String
+
+  def palindrome?( str=nil )
+
+    # I do not know why this does not work
+    #  str = str.gsub( /[áàäÁÀÄ]/, "a" ) #remove variations a-es
+    #  str = str.gsub( /[éèëÉÈË]/, "e" ) #remove variations e-es
+    #  str = str.gsub( /[íìïÍÌÏ]/, "i" ) #remove variations i-es
+    #  str = str.gsub( /[óòöÓÒÖ]/, "o" ) #remove variations o-es
+    #  str = str.gsub( /[úùüÚÙÜ]/, "u" ) #remove variations u-es
+
+    str = self if str==nil
+
+    str = variations_a_es str #remove variations a-es
+    str = variations_e_es str #remove variations e-es
+    str = variations_i_es str #remove variations i-es
+    str = variations_o_es str #remove variations o-es
+    str = variations_u_es str #remove variations u-es
+
+    str = str.gsub( /[\W_0-9]/, "" )
+  
+    if 0 == str.casecmp( str.reverse )
+      true
+    else
+      false
+    end
+  end
+end
+
+
+
 #---------------test--------------#
 
 #Numeric.add_currency 'dollar', 1.0
@@ -49,9 +127,6 @@ Numeric.add_currency 'peso', 0.5
 print "15.yen.in :pesos "
 puts 15.yen.in :pesos
 
-class String
-  # YOUR CODE HERE
-end
 
 module Enumerable
   # YOUR CODE HERE
