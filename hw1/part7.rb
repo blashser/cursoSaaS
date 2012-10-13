3cd7f59bea3852dad4df15acd12aa79c673a7b10
Sergio Hernández <blashser.aero@gmail.com>
2012-10-13 03:26:34 +0200

    Update testing part
    readable code.

--- hw1/part7.rb
+++ hw1/part7.rb
@@ -28,22 +28,13 @@ array_nulo  = []
 array_corto = [ 3, "casa" ]
 array_largo = [ :who, 25, [ 3, { 2 => "cinco", :jo => 20} ], 'nino' ]
 
-ab = CartesianProduct.new array_corto, array_largo
-
-puts ab.product.inspect
-
-bc = CartesianProduct.new array_nulo, array_corto
-
-puts bc.product.inspect
-
-cd = CartesianProduct.new array_largo, array_nulo
-
-puts cd.product.inspect
+ab = CartesianProduct.new array_corto, array_largo; puts ab.product.inspect
+bc = CartesianProduct.new array_nulo,  array_corto; puts bc.product.inspect
+cd = CartesianProduct.new array_largo, array_nulo;  puts cd.product.inspect
 
 cd.each { |j| puts j.inspect }
 
 [ ab, bc, cd ].each do |de|
-  de.each do |veras|
-    puts veras.inspect
-  end
+  puts "testing"
+  de.each { |veras| puts veras.inspect }
 end
