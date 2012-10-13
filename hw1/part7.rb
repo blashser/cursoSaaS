e1675945e985ab8c74b8cff664c21e89cc0eb7a7
Sergio Hernández <blashser.aero@gmail.com>
2012-10-13 03:39:54 +0200

    Remove part used to test
    Before submit files to check it is said that test
    part must be removed.

--- hw1/part7.rb
+++ hw1/part7.rb
@@ -22,20 +22,3 @@ class CartesianProduct
   attr_reader :product
 end
 
-
-#----------test----------#
-
-array_nulo  = []
-array_corto = [ 3, "casa" ]
-array_largo = [ :who, 25, [ 3, { 2 => "cinco", :jo => 20} ], 'nino' ]
-
-ab = CartesianProduct.new array_corto, array_largo; #puts ab.product.inspect
-bc = CartesianProduct.new array_nulo,  array_corto; #puts bc.product.inspect
-cd = CartesianProduct.new array_largo, array_nulo;  #puts cd.product.inspect
-
-cd.each { |j| puts j.inspect }
-
-[ ab, bc, cd ].each do |de|
-  puts "testing"
-  de.each { |veras| puts veras.inspect }
-end
