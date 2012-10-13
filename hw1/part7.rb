fd26d3666d8418b9fad72261bae16dc0615a7562
Sergio Hernández <blashser.aero@gmail.com>
2012-10-13 03:05:52 +0200

    First version of initialize
    Do Accessor for variable and a constructor.

--- hw1/part7.rb
+++ hw1/part7.rb
@@ -1,4 +1,16 @@
 class CartesianProduct
   include Enumerable
-  # YOUR CODE HERE
+
+  attr_accessor :product
+
+  def initialize( arry1, arry2 )
+    unless arry1.empty? || arry2.empty?
+      @product = []
+      arry1.each do |first|
+        arry2.each do |second|
+          @product << [ first, second ]
+        end
+      end
+    end
+  end
 end
