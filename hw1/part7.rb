9af2b528d3fb3db75a299146a342529ea665000e
Sergio Hernández <blashser.aero@gmail.com>
2012-10-13 03:06:43 +0200

    Test part
    Check what is expected in the Cartesian product objects.

--- hw1/part7.rb
+++ hw1/part7.rb
@@ -14,3 +14,22 @@ class CartesianProduct
     end
   end
 end
+
+
+#----------test----------#
+
+array_nulo  = []
+array_corto = [ 3, "casa" ]
+array_largo = [ :who, 25, [ 3, { 2 => "cinco", :jo => 20} ], 'nino' ]
+
+ab = CartesianProduct.new array_corto, array_largo
+
+puts ab.product.inspect
+
+bc = CartesianProduct.new array_nulo, array_corto
+
+puts bc.product.inspect
+
+cd = CartesianProduct.new array_largo, array_nulo
+
+puts cd.product.inspect
